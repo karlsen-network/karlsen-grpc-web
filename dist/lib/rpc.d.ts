d21b5483b9a8713faf9e94e9d53d360770f6b6c2
surinder singh <surinder83singh@gmail.com>
2020-11-20 19:24:37 +0530

    rpc processQueue

--- dist/lib/rpc.d.ts
+++ dist/lib/rpc.d.ts
@@ -1,14 +1,25 @@
 /// <reference types="flow-grpc-web" />
 import { FlowGRPCWeb } from '@aspectron/flow-grpc-web';
 import { IRPC, Api } from '../types/custom-types';
+interface QueueItem {
+    method: string;
+    data: any;
+    resolve: Function;
+    reject: Function;
+}
 export declare class RPC implements IRPC {
     isReady: boolean;
     client: FlowGRPCWeb;
+    stream: any;
+    queue: QueueItem[];
+    pending: QueueItem[];
     constructor(options?: any);
-    request(method: string, req: any, resolve: Function, reject: Function): void;
+    processQueue(): void;
+    request(method: string, data: any, resolve: Function, reject: Function): void;
     getBlock(blockHash: string): Promise<Api.BlockResponse>;
     getAddressTransactions(address: string, limit: number, skip: number): Promise<Api.Transaction[]>;
     getUtxos(address: string, limit: number, skip: number): Promise<Api.Utxo[]>;
     postTx(rawTransaction: string): Promise<Api.SuccessResponse>;
 }
+export {};
 //# sourceMappingURL=rpc.d.ts.map
--- lib/rpc.ts
+++ lib/rpc.ts
@@ -1,30 +1,60 @@
 import {FlowGRPCWeb} from '@aspectron/flow-grpc-web';
 //const FlowGRPCWeb = new FlowGRPCWeb();
 import {IRPC, Api} from '../types/custom-types';
+interface QueueItem{
+	method:string,
+	data:any,
+	resolve:Function,
+	reject:Function
+}
+
 export class RPC implements IRPC{
 	isReady:boolean = false;
 	client:FlowGRPCWeb;
 	stream:any;
+	queue:QueueItem[] = [];
+	pending:QueueItem[] = [];
+
 	constructor(options:any={}){
 		this.client = new FlowGRPCWeb(options.grpc||{});
+
 		this.client.on("ready", (clients:any)=>{
 			console.log("gRPCWeb::::clients", clients)
 			let {RPC} = clients;
 
 			const stream = RPC.MessageStream();
+			this.stream = stream;
 			console.log("stream", stream)
 			stream.on("data", (data:string)=>{
 				console.log("stream data", data)
+				let item:QueueItem|undefined = this.pending.shift();
+				if(item){
+					item.resolve(data);
+				}
 			})
 			stream.on("end", ()=>{
 				console.log("stream end")
 			});
 
 			this.isReady = true;
+			this.processQueue();
 		})
 	}
-	request(method:string, req:any, resolve:Function, reject:Function){
-		
+	processQueue(){
+		if(!this.isReady)
+			return
+		let item:QueueItem|undefined = this.queue.shift();
+		while(item){
+			let req:any = {};
+			req[item.method] = item.data;
+			this.pending.push(item);
+			this.stream.write(req);
+			item = this.queue.shift();
+		}
+	}
+	request(method:string, data:any, resolve:Function, reject:Function){
+		this.queue.push({method, data, resolve, reject});
+		this.processQueue();
 	}
 	getBlock(blockHash:string): Promise<Api.BlockResponse>{
 		return new Promise((resolve, reject)=>{
