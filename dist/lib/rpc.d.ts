f30b3cf8fb06126aad8f5de62097d99a627a71c6
surinder singh <surinder83singh@gmail.com>
2021-02-11 21:17:34 +0530

    added missing functions

--- dist/lib/rpc.d.ts
+++ dist/lib/rpc.d.ts
@@ -1,26 +1,55 @@
 /// <reference types="flow-grpc-web" />
 import { FlowGRPCWeb } from '@aspectron/flow-grpc-web';
 import { IRPC, RPC as Rpc, SubscriberItemMap, QueueItem, PendingReqs, IData, IStream } from '../types/custom-types';
+export interface DeferredPromise extends Promise<any> {
+    resolve(data?: any): void;
+    reject(error?: any): void;
+}
+export declare const Deferred: () => DeferredPromise;
 export declare class RPC implements IRPC {
     isReady: boolean;
+    options: any;
     client: FlowGRPCWeb;
+    reconnect_dpc: number | undefined;
     stream: IStream;
     queue: QueueItem[];
     pending: PendingReqs;
     intakeHandler: Function | undefined;
+    reconnect: boolean;
     verbose: boolean;
     subscribers: SubscriberItemMap;
+    isConnected: boolean;
+    connectCBs: Function[];
+    connectFailureCBs: Function[];
+    errorCBs: Function[];
+    disconnectCBs: Function[];
+    serviceClient: any;
+    serviceClientSignal: DeferredPromise;
+    log: Function;
     constructor(options?: any);
+    getServiceClient(): Promise<any>;
+    connect(): Promise<void>;
+    _connect(): Promise<void>;
     initIntake(stream: IStream): void;
     handleIntake(o: IData): void;
     setIntakeHandler(fn: Function): void;
     processQueue(): void;
     clearPending(): void;
-    subscribe<T, R>(subject: string, data: any, callback: Rpc.callback<R>): Rpc.SubPromise<T>;
+    _setConnected(isConnected: boolean): void;
+    onConnect(callback: Function): void;
+    onConnectFailure(callback: Function): void;
+    onError(callback: Function): void;
+    onDisconnect(callback: Function): void;
+    disconnect(): void;
     request<T>(method: string, data: any): Promise<T>;
+    subscribe<T, R>(subject: string, data: any, callback: Rpc.callback<R>): Rpc.SubPromise<T>;
+    subject2EventName(subject: string): string;
+    unSubscribe(subject: string, uid?: string): void;
     subscribeChainChanged(callback: Rpc.callback<Rpc.ChainChangedNotification>): Rpc.SubPromise<Rpc.NotifyChainChangedResponse>;
     subscribeBlockAdded(callback: Rpc.callback<Rpc.BlockAddedNotification>): Rpc.SubPromise<Rpc.NotifyBlockAddedResponse>;
     subscribeVirtualSelectedParentBlueScoreChanged(callback: Rpc.callback<Rpc.VirtualSelectedParentBlueScoreChangedNotification>): Rpc.SubPromise<Rpc.NotifyVirtualSelectedParentBlueScoreChangedResponse>;
+    subscribeUtxosChanged(addresses: string[], callback: Rpc.callback<Rpc.UtxosChangedNotification>): Rpc.SubPromise<Rpc.NotifyUtxosChangedResponse>;
+    unSubscribeUtxosChanged(uid?: string): void;
     getBlock(hash: string): Promise<Rpc.BlockResponse>;
     getTransactionsByAddresses(startingBlockHash: string, addresses: string[]): Promise<Rpc.TransactionsByAddressesResponse>;
     getUtxosByAddresses(addresses: string[]): Promise<Rpc.UTXOsByAddressesResponse>;
--- lib/rpc.ts
+++ lib/rpc.ts
@@ -1,28 +1,70 @@
-import {FlowGRPCWeb} from '@aspectron/flow-grpc-web';
+import {FlowGRPCWeb, dpc, clearDPC} from '@aspectron/flow-grpc-web';
 //const FlowGRPCWeb = new FlowGRPCWeb();
 import {IRPC, RPC as Rpc,
 	SubscriberItem, SubscriberItemMap,
 	QueueItem, PendingReqs, IData, IStream
 } from '../types/custom-types';
 
+export interface DeferredPromise extends Promise<any> {
+    resolve(data?:any):void;
+    reject(error?:any):void;
+}
+export const Deferred = (): DeferredPromise=>{
+    let methods = {};
+    let promise = new Promise((resolve, reject)=>{
+        methods = {resolve, reject};
+    })
+    Object.assign(promise, methods);
+    return promise as DeferredPromise;
+}
+
 export class RPC implements IRPC{
 	isReady:boolean = false;
+	options:any;
 	client:FlowGRPCWeb;
+	reconnect_dpc:number|undefined;
 	stream:IStream;
 	queue:QueueItem[] = [];
 	pending:PendingReqs;
 	intakeHandler:Function|undefined;
+	reconnect:boolean = true;
 	verbose:boolean = false;
 	subscribers: SubscriberItemMap = new Map();
+	isConnected:boolean=false;
+	connectCBs:Function[] = [];
+	connectFailureCBs:Function[] = [];
+	errorCBs:Function[] = [];
+	disconnectCBs:Function[] = [];
+	serviceClient:any;
+	serviceClientSignal: DeferredPromise;
+	log:Function;
 
 	constructor(options:any={}){
+		this.options = Object.assign({
+			reconnect: true,
+			verbose : false,
+			uid:(Math.random()*1000).toFixed(0)
+		}, options||{});
+
+		this.log = Function.prototype.bind.call(
+			console.log,
+			console,
+			`[Kaspa gRPC ${this.options.uid}]:`
+		);
+
 		this.pending = {};
-		this.client = new FlowGRPCWeb(options.grpc||{});
+		this.reconnect = this.options.reconnect;
+		this.client = new FlowGRPCWeb(options.clientConfig||{});
+
+		this.serviceClientSignal = Deferred();
 
 		this.client.on("ready", (clients:any)=>{
 			console.log("gRPCWeb::::clients", clients)
 			let {RPC} = clients;
+			this.serviceClient = RPC;
+			this.serviceClientSignal.resolve();
 
+			/*
 			const stream = RPC.MessageStream();
 			this.stream = stream;
 			console.log("stream", stream)
@@ -30,12 +72,72 @@ export class RPC implements IRPC{
 				console.log("stream end")
 			});
 			this.initIntake(stream);
-			this.isReady = true;
-			this.processQueue();
+			*/
+		})
+		this.connect();
+	}
+	async getServiceClient(){
+		await this.serviceClientSignal;
+		return this.serviceClient;
+	}
+	connect(){
+		this.reconnect = true;
+		return this._connect();
+	}
+	async _connect() {
+		// this.reconnect = true;
+		this.verbose && this.log('gRPC Client connecting to', this.options.host);
+		const RPC = await this.getServiceClient();
+
+		this.stream = RPC.MessageStream();
+		this.initIntake(this.stream);
+		this.isReady = true;
+		this.processQueue();
+
+		const reconnect = () => {
+			this._setConnected(false);
+			if(this.reconnect_dpc) {
+				clearDPC(this.reconnect_dpc);
+				delete this.reconnect_dpc;
+			}
+
+			this.clearPending();
+			delete this.stream;
+			//delete this.client;
+			if(this.reconnect) {
+				this.reconnect_dpc = dpc(1000, () => {
+					this._connect();
+				})
+			}
+		}
+		this.stream.on('error', (error:any) => {
+			// console.log("client:",error);
+			this.errorCBs.forEach(fn=>fn(error.toString(), error));
+			this.verbose && this.log('stream:error', error);
+			reconnect();
+		})
+		this.stream.on('end', (...args:any) => {
+			this.verbose && this.log('stream:end', ...args);
+			reconnect();
+		});
+
+		await new Promise<void>((resolve)=>{
+			dpc(100, async()=>{
+				let response:any = await this.request('getVirtualSelectedParentBlueScoreRequest', {})
+				.catch(e=>{
+					this.connectFailureCBs.forEach(fn=>fn(e));
+				})
+				this.verbose && this.log("getVirtualSelectedParentBlueScoreRequest:response", response)
+				if(response && response.blueScore){
+					this._setConnected(true);
+				}
+				resolve();
+			})
 		})
 	}
 	initIntake(stream:IStream) {
         stream.on('data',(data:any) => {
+        	this.log("initIntake:data", data)
             if(data.payload) {
                 let name = data.payload;
                 let payload = data[name];
@@ -94,6 +196,49 @@ export class RPC implements IRPC{
             this.pending[key] = [];
         });
     }
+
+    _setConnected(isConnected:boolean){
+		if(this.isConnected == isConnected)
+			return;
+		this.isConnected = isConnected;
+
+		let cbs = isConnected?this.connectCBs:this.disconnectCBs;
+		//console.log("this.isConnected", this.isConnected, cbs)
+		cbs.forEach(fn=>{
+			fn();
+		})
+	}
+
+	onConnect(callback:Function){
+		this.connectCBs.push(callback)
+		if(this.isConnected)
+			callback();
+	}
+	onConnectFailure(callback:Function){
+		this.connectFailureCBs.push(callback)
+	}
+	onError(callback:Function){
+		this.errorCBs.push(callback)
+	}
+	onDisconnect(callback:Function){
+		this.disconnectCBs.push(callback)
+	}
+
+	disconnect() {
+		if(this.reconnect_dpc) {
+			clearDPC(this.reconnect_dpc);
+			delete this.reconnect_dpc;
+		}
+		this.reconnect = false;
+		this.stream && this.stream.end();
+		this.clearPending();
+	}
+	request<T>(method:string, data:any){
+		return new Promise<T>((resolve, reject)=>{
+			this.queue.push({method, data, resolve, reject});
+			this.processQueue();
+		})
+	}
     subscribe<T, R>(subject:string, data:any={}, callback:Rpc.callback<R>):Rpc.SubPromise<T>{
 		if(typeof data == 'function'){
 			callback = data;
@@ -103,8 +248,7 @@ export class RPC implements IRPC{
 		if(!this.client)
 			return Promise.reject('not connected') as Rpc.SubPromise<T>;
 
-		let eventName = subject.replace("notify", "").replace("Request", "Notification")
-		eventName = eventName[0].toLowerCase()+eventName.substr(1);
+		let eventName = this.subject2EventName(subject);
 		console.log("subscribe:eventName", eventName)
 
 		let subscribers:SubscriberItem[]|undefined = this.subscribers.get(eventName);
@@ -120,11 +264,22 @@ export class RPC implements IRPC{
 		p.uid = uid;
 		return p;
 	}
-	request<T>(method:string, data:any){
-		return new Promise<T>((resolve, reject)=>{
-			this.queue.push({method, data, resolve, reject});
-			this.processQueue();
-		})
+	subject2EventName(subject:string){
+		let eventName = subject.replace("notify", "").replace("Request", "Notification")
+		return eventName[0].toLowerCase()+eventName.substr(1);
+	}
+
+	unSubscribe(subject:string, uid:string=''){
+		let eventName = this.subject2EventName(subject);
+		let subscribers:SubscriberItem[]|undefined = this.subscribers.get(eventName);
+		if(!subscribers)
+			return
+		if(!uid){
+			this.subscribers.delete(eventName);
+		}else{
+			subscribers = subscribers.filter(sub=>sub.uid!=uid)
+			this.subscribers.set(eventName, subscribers);
+		}
 	}
 
 	subscribeChainChanged(callback:Rpc.callback<Rpc.ChainChangedNotification>){
@@ -137,11 +292,21 @@ export class RPC implements IRPC{
 		return this.subscribe<Rpc.NotifyVirtualSelectedParentBlueScoreChangedResponse, Rpc.VirtualSelectedParentBlueScoreChangedNotification>("notifyVirtualSelectedParentBlueScoreChangedRequest", {}, callback);
 	}
 
+	subscribeUtxosChanged(addresses:string[], callback:Rpc.callback<Rpc.UtxosChangedNotification>){
+		return this.subscribe<Rpc.NotifyUtxosChangedResponse, Rpc.UtxosChangedNotification>("notifyUtxosChangedRequest", {addresses}, callback);
+	}
+
+	unSubscribeUtxosChanged(uid:string=''){
+		this.unSubscribe("notifyUtxosChangedRequest", uid);
+	}
+
 	getBlock(hash:string){
 		return this.request<Rpc.BlockResponse>('getBlockRequest', {hash, includeBlockVerboseData:true});
 	}
 	getTransactionsByAddresses(startingBlockHash:string, addresses:string[]){
-		return this.request<Rpc.TransactionsByAddressesResponse>('getTransactionsByAddressesRequest', {startingBlockHash, addresses});
+		return this.request<Rpc.TransactionsByAddressesResponse>('getTransactionsByAddressesRequest', {
+			startingBlockHash, addresses
+		});
 	}
 	getUtxosByAddresses(addresses:string[]){
 		return this.request<Rpc.UTXOsByAddressesResponse>('getUtxosByAddressesRequest', {addresses});
@@ -149,6 +314,7 @@ export class RPC implements IRPC{
 	submitTransaction(tx: Rpc.SubmitTransactionRequest){
 		return this.request<Rpc.SubmitTransactionResponse>('submitTransactionRequest', tx);
 	}
+
 	getVirtualSelectedParentBlueScore(){
 		return this.request<Rpc.VirtualSelectedParentBlueScoreResponse>('getVirtualSelectedParentBlueScoreRequest', {});
 	}
